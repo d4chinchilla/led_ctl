81026c0a12354cf09b5536cb7d383c10eb9cdae8
Mark Warnants <mark.warnants@gmail.com>
2019-02-22 16:40:59 +0000

    Completed LED code
    need to add calibration button code

--- Version1.py
+++ Version1.py
@@ -7,17 +7,15 @@ import json
 
 # Neopixel setup
 pixel_pin = board.D18
-num_pixels = 25
+num_pixels = 60
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
 # Define pi
 pi = 3.14159265359
 
-# Declare Colour memory
-red = 1
-green = 0
-blue = 0
+# Declare id memory
+last_id = 0
 
 # Declare position and offset on LED ring and offset signedness
 ring_pos = 0
@@ -37,32 +35,41 @@ def led_ring(angle, amplitude):
         offset_sign = 0
 
     ring_offset = abs(ring_offset)
-
-    for i in range(0, 256):
-        pixles[ring_pos + (-2 * offset_sign)] = (( (round(i * amplitude * red * (1 - ring_offset) / 3 )), (round(i * amplitude * green * (1 - ring_offset) / 3 )), (round(i * amplitude * blue * (1 - ring_offset) / 3 )) ))
-        pixles[ring_pos + (-1 * offset_sign)] = (( (round(i * amplitude * red * (2 - ring_offset) / 3 )), (round(i * amplitude * green * (2 - ring_offset) / 3 )), (round(i * amplitude * blue * (2 - ring_offset) / 3 )) ))
-        pixels[ring_pos + ( 0 * offset_sign)] = (( (round(i * amplitude * red * (3 - ring_offset) / 3 )), (round(i * amplitude * green * (3 - ring_offset) / 3 )), (round(i * amplitude * blue * (3 - ring_offset) / 3 )) ))
-        pixels[ring_pos + ( 1 * offset_sign)] = (( (round(i * amplitude * red * (2 + ring_offset) / 3 )), (round(i * amplitude * green * (2 + ring_offset) / 3 )), (round(i * amplitude * blue * (2 + ring_offset) / 3 )) ))
-        pixles[ring_pos + ( 2 * offset_sign)] = (( (round(i * amplitude * red * (1 + ring_offset) / 3 )), (round(i * amplitude * green * (1 + ring_offset) / 3 )), (round(i * amplitude * blue * (1 + ring_offset) / 3 )) ))
-        pixles[ring_pos + ( 3 * offset_sign)] = (( (round(i * amplitude * red * (0 + ring_offset) / 3 )), (round(i * amplitude * green * (0 + ring_offset) / 3 )), (round(i * amplitude * blue * (0 + ring_offset) / 3 )) ))
-
-        pixels.show()
-        time.sleep(0.002)
-
-        if red == 1:
-            red = 0
-            green = 1
-            blue = 0
-        elif green == 1:
-            red = 0
-            green = 0
-            blue = 1
-        else:
-            red = 1
-            green = 0
-            blue = 0
+    pixles[ring_pos + (-2 * offset_sign)] = (( (amplitude * max(0, ((2 * amplitude) - 1)) * (1 - ring_offset) / 3 ),
+                                               (amplitude * (1 - abs((2 * amplitude) - 1)) * (1 - ring_offset) / 3 ),
+                                               (amplitude * max(0, ((-2 * amplitude) + 1)) * (1 - ring_offset) / 3 ) ))
+    pixles[ring_pos + (-1 * offset_sign)] = (( (amplitude * max(0, ((2 * amplitude) - 1)) * (2 - ring_offset) / 3 ),
+                                               (amplitude * (1 - abs((2 * amplitude) - 1)) * (2 - ring_offset) / 3 ),
+                                               (amplitude * max(0, ((-2 * amplitude) + 1)) * (2 - ring_offset) / 3 ) ))
+    pixels[ring_pos + ( 0 * offset_sign)] = (( (amplitude * max(0, ((2 * amplitude) - 1)) * (3 - ring_offset) / 3 ),
+                                               (amplitude * (1 - abs((2 * amplitude) - 1)) * (3 - ring_offset) / 3 ),
+                                               (amplitude * max(0, ((-2 * amplitude) + 1)) * (3 - ring_offset) / 3 ) ))
+    pixels[ring_pos + ( 1 * offset_sign)] = (( (amplitude * max(0, ((2 * amplitude) - 1)) * (2 + ring_offset) / 3 ),
+                                               (amplitude * (1 - abs((2 * amplitude) - 1)) * (2 + ring_offset) / 3 ),
+                                               (amplitude * max(0, ((-2 * amplitude) + 1)) * (2 + ring_offset) / 3 ) ))
+    pixles[ring_pos + ( 2 * offset_sign)] = (( (amplitude * max(0, ((2 * amplitude) - 1)) * (1 + ring_offset) / 3 ),
+                                               (amplitude * (1 - abs((2 * amplitude) - 1)) * (1 + ring_offset) / 3 ),
+                                               (amplitude * max(0, ((-2 * amplitude) + 1)) * (1 + ring_offset) / 3 ) ))
+    pixles[ring_pos + ( 3 * offset_sign)] = (( (amplitude * max(0, ((2 * amplitude) - 1)) * ring_offset / 3 ),
+                                               (amplitude * (1 - abs((2 * amplitude) - 1)) * ring_offset / 3 ),
+                                               (amplitude * max(0, ((-2 * amplitude) + 1)) * ring_offset / 3 ) ))
+    pixels.show()
 
 # Main loop
 while True:
-        with open('/tmp/chinchilla-backend.json', 'r') as json_file:  
-                data = json.load(json_file)
+    for i in range(0, num_pixels):
+        if pixels[i] > 0:
+            pixels[i] = pixels[i] - 1
+    
+    with open('/tmp/chinchilla-backend.json', 'r') as json_file:
+        for line in json_file.readlines():
+            object = json.loads(line)
+            var id = object.id
+            var angle = object.angle
+            var amplitude = object.amplitude
+                
+            if id > last_id:
+                led_ring(angle, amplitude)
+                last_id = id
+            
+            time.sleep(0.002)
