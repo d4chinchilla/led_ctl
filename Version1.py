a69b13b442fea1eda56880b4614dfda66f3e966a
Mark Warnants <mark.warnants@gmail.com>
2019-02-25 13:10:52 +0000

    Finished LED code
    Improved formatting, need to check works & button needs to be added

--- Version1.py
+++ Version1.py
@@ -1,15 +1,21 @@
-# Based on example by adafruit
+# Based on example by Adafruit and using Adafruit libraries
+ 
+# _____________________________________________________________________________
+# Setup
+# _____________________________________________________________________________
+
 import time
 import board
 import neopixel
 import json
 import digitalio
 
-# Neopixel setup
+# LED setup
 pixel_pin = board.D18
 num_pixels = 60
 ORDER = neopixel.GRB
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=ORDER)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1,
+                           auto_write=False, pixel_order=ORDER)
 
 # Button setup
 button = digitalio.DigitalInOut(board.D4)
@@ -22,12 +28,22 @@ pi = 3.14159265359
 # Declare id memory
 last_id = 0
 
+# Declare LED indices
+index_2 = 0
+index_1 = 0
+index0 = 0
+index1 = 0
+index2 = 0
+index3 = 0
+
 # Declare position and offset on LED ring and offset signedness
 ring_pos = 0
 ring_offset = 0
 offset_sign = 0
 
-# Led ring code
+# _____________________________________________________________________________
+# LED ring code
+# _____________________________________________________________________________
 def led_ring(angle, amplitude):
 	
 	# Translate angle to LED number
@@ -44,33 +60,112 @@ def led_ring(angle, amplitude):
     ring_offset = abs(ring_offset)
     
     # Work out LED values
-    if (ring_pos + (-2 * offset_sign)) > -1 and (ring_pos + (-2 * offset_sign)) < num_pixels:
-        pixels[ring_pos + (-2 * offset_sign)] = (( round(amplitude * 255 * max(0, ((2 * amplitude) - 1)) * (1 - ring_offset) / 3 ),
-                                                   round(amplitude * 255 * (1 - abs((2 * amplitude) - 1)) * (1 - ring_offset) / 3 ),
-                                                   round(amplitude * 255 * max(0, ((-2 * amplitude) + 1)) * (1 - ring_offset) / 3 ) ))
-    if (ring_pos + (-1 * offset_sign)) > -1 and (ring_pos + (-1 * offset_sign)) < num_pixels:
-        pixels[ring_pos + (-1 * offset_sign)] = (( round(amplitude * 255 * max(0, ((2 * amplitude) - 1)) * (2 - ring_offset) / 3 ),
-                                                   round(amplitude * 255 * (1 - abs((2 * amplitude) - 1)) * (2 - ring_offset) / 3 ),
-                                                   round(amplitude * 255 * max(0, ((-2 * amplitude) + 1)) * (2 - ring_offset) / 3 ) ))
-    if (ring_pos + ( 0 * offset_sign)) > -1 and (ring_pos + ( 0 * offset_sign)) < num_pixels:
-        pixels[ring_pos + ( 0 * offset_sign)] = (( round(amplitude * 255 * max(0, ((2 * amplitude) - 1)) * (3 - ring_offset) / 3 ),
-                                                   round(amplitude * 255 * (1 - abs((2 * amplitude) - 1)) * (3 - ring_offset) / 3 ),
-                                                   round(amplitude * 255 * max(0, ((-2 * amplitude) + 1)) * (3 - ring_offset) / 3 ) ))
-    if (ring_pos + ( 1 * offset_sign)) > -1 and (ring_pos + ( 1 * offset_sign)) < num_pixels:
-        pixels[ring_pos + ( 1 * offset_sign)] = (( round(amplitude * 255 * max(0, ((2 * amplitude) - 1)) * (2 + ring_offset) / 3 ),
-                                                   round(amplitude * 255 * (1 - abs((2 * amplitude) - 1)) * (2 + ring_offset) / 3 ),
-                                                   round(amplitude * 255 * max(0, ((-2 * amplitude) + 1)) * (2 + ring_offset) / 3 ) ))
-    if (ring_pos + ( 2 * offset_sign)) > -1 and (ring_pos + ( 2 * offset_sign)) < num_pixels:
-        pixels[ring_pos + ( 2 * offset_sign)] = (( round(amplitude * 255 * max(0, ((2 * amplitude) - 1)) * (1 + ring_offset) / 3 ),
-                                                   round(amplitude * 255 * (1 - abs((2 * amplitude) - 1)) * (1 + ring_offset) / 3 ),
-                                                   round(amplitude * 255 * max(0, ((-2 * amplitude) + 1)) * (1 + ring_offset) / 3 ) ))
-    if (ring_pos + ( 3 * offset_sign)) > -1 and (ring_pos + ( 3 * offset_sign)) < num_pixels:
-        pixels[ring_pos + ( 3 * offset_sign)] = (( round(amplitude * 255 * max(0, ((2 * amplitude) - 1)) * ring_offset / 3 ),
-                                                   round(amplitude * 255 * (1 - abs((2 * amplitude) - 1)) * ring_offset / 3 ),
-                                                   round(amplitude * 255 * max(0, ((-2 * amplitude) + 1)) * ring_offset / 3 ) ))
+    if (ring_pos + (-2 * offset_sign)) < 0:
+        index_2 = ring_pos + (-2 * offset_sign) + num_pixels
+    elif (ring_pos + (-2 * offset_sign)) >= num_pixels:
+        index_2 = ring_pos + (-2 * offset_sign) - num_pixels)
+    else:
+        index_2 = ring_pos + (-2 * offset_sign)
+    
+    pixel = list(pixels[index_2])
+
+    pixels[index_2] = (( min(255, pixel[0] + round(amplitude * 255 *
+        max(0, ((2 * amplitude) - 1)) * (1 - ring_offset) / 3 )),
+                         min(255, pixel[1] + round(amplitude * 255 *
+        (1 - abs((2 * amplitude) - 1)) * (1 - ring_offset) / 3 )),
+                         min(255, pixel[2] + round(amplitude * 255 *
+        max(0, ((-2 * amplitude) + 1)) * (1 - ring_offset) / 3 )) ))
+    
+    
+    if (ring_pos - offset_sign) < 0:
+        index_1 = ring_pos - offset_sign + num_pixels
+    elif (ring_pos - offset_sign) >= num_pixels:
+        index_1 = ring_pos - offset_sign - num_pixels)
+    else:
+        index_1 = ring_pos - offset_sign
+    
+    pixel = list(pixels[index_1])
+    
+    pixels[index_1] = (( min(255, pixel[0] + round(amplitude * 255 *
+        max(0, ((2 * amplitude) - 1)) * (2 - ring_offset) / 3 )),
+                         min(255, pixel[1] + round(amplitude * 255 *
+        (1 - abs((2 * amplitude) - 1)) * (2 - ring_offset) / 3 )),
+                         min(255, pixel[2] + round(amplitude * 255 *
+        max(0, ((-2 * amplitude) + 1)) * (2 - ring_offset) / 3 )) ))
+    
+    
+    if ring_pos < 0:
+        index0 = ring_pos + num_pixels
+    elif ring_pos >= num_pixels:
+        index0 = ring_pos - num_pixels
+    else:
+        index0 = ring_pos
+    
+    pixel = list(pixels[index0])
+    
+    pixels[index0] = (( min(255, pixel[0] + round(amplitude * 255 *
+        max(0, ((2 * amplitude) - 1)) * (3 - ring_offset) / 3 )),
+                        min(255, pixel[1] + round(amplitude * 255 *
+        (1 - abs((2 * amplitude) - 1)) * (3 - ring_offset) / 3 )),
+                        min(255, pixel[2] + round(amplitude * 255 *
+        max(0, ((-2 * amplitude) + 1)) * (3 - ring_offset) / 3 )) ))
+    
+    
+    if (ring_pos + offset_sign) < 0:
+        index1 = ring_pos + offset_sign + num_pixels
+    elif (ring_pos + offset_sign) >= num_pixels:
+        index1 = ring_pos + offset_sign - num_pixels)
+    else:
+        index1 = ring_pos + offset_sign
+    
+    pixel = list(pixels[index1])
+    
+    pixels[index1] = (( min(255, pixel[0] + round(amplitude * 255 *
+        max(0, ((2 * amplitude) - 1)) * (2 + ring_offset) / 3 )),
+                        min(255, pixel[1] + round(amplitude * 255 *
+        (1 - abs((2 * amplitude) - 1)) * (2 + ring_offset) / 3 )),
+                        min(255, pixel[2] + round(amplitude * 255 *
+        max(0, ((-2 * amplitude) + 1)) * (2 + ring_offset) / 3 )) ))
+    
+    
+    if (ring_pos + (2 * offset_sign)) < 0:
+        index2 = ring_pos + (2 * offset_sign) + num_pixels
+    elif (ring_pos + (2 * offset_sign)) >= num_pixels:
+        index2 = ring_pos + (2 * offset_sign) - num_pixels)
+    else:
+        index2 = ring_pos + (2 * offset_sign)
+    
+    pixel = list(pixels[index2])
+    
+    pixels[index2] = (( min(255, pixel[0] + round(amplitude * 255 *
+        max(0, ((2 * amplitude) - 1)) * (1 + ring_offset) / 3 )),
+                        min(255, pixel[1] + round(amplitude * 255 *
+        (1 - abs((2 * amplitude) - 1)) * (1 + ring_offset) / 3 )),
+                        min(255, pixel[2] + round(amplitude * 255 *
+        max(0, ((-2 * amplitude) + 1)) * (1 + ring_offset) / 3 )) ))
+    
+    
+    if (ring_pos + (3 * offset_sign)) < 0:
+        index3 = ring_pos + (3 * offset_sign) + num_pixels
+    elif (ring_pos + (3 * offset_sign)) >= num_pixels:
+        index3 = ring_pos + (3 * offset_sign) - num_pixels)
+    else:
+        index3 = ring_pos + (3 * offset_sign)
+    
+    pixel = list(pixels[index3])
+    
+    pixels[index3] = (( min(255, pixel[0] + round(amplitude * 255 *
+        max(0, ((2 * amplitude) - 1)) * ring_offset / 3 )),
+                        min(255, pixel[1] + round(amplitude * 255 *
+        (1 - abs((2 * amplitude) - 1)) * ring_offset / 3 )),
+                        min(255, pixel[2] + round(amplitude * 255 *
+        max(0, ((-2 * amplitude) + 1)) * ring_offset / 3 )) ))
+    
     pixels.show()
 
+# _____________________________________________________________________________
 # Main loop
+# _____________________________________________________________________________
 while True:
 	
 	# Fade LEDs out
@@ -89,9 +184,9 @@ while True:
     with open('/tmp/chinchilla-backend.json', 'r') as json_file:
         for line in json_file.readlines():
             object = json.loads(line)
-            id = object["id"]
-            angle = object["angle"]
-            amplitude = object["amplitude"]
+            id = object['id']
+            angle = object['angle']
+            amplitude = object['amplitude']
             
             if id > last_id:
                 led_ring(angle, amplitude)
