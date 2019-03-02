a3641bfdabc705db170e08db960fd83334f9f876
Mark Warnants <mark.warnants@gmail.com>
2019-03-02 18:29:34 +0000

    Update no. LEDs

--- Version1.py
+++ Version1.py
@@ -12,7 +12,7 @@ import digitalio
 
 # LED setup
 pixel_pin = board.D18
-num_pixels = 60
+num_pixels = 46
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1,
                            auto_write=False, pixel_order=ORDER)
