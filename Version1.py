980604c6d347652852338287bd8f93c0d723b7b9
Mark Warnants <mark.warnants@gmail.com>
2019-03-03 22:25:54 +0000

    Generalised fanout for LEDs and commented

--- Version1.py
+++ Version1.py
@@ -2,13 +2,21 @@
  
 # _____________________________________________________________________________
 # Setup
+#
+# Import libraries
+# Setup LEDs for Adafruit library
+# Setup button and declare as pull up input
+# Setup memory for remembering id, time and button state between loops
+# Declare the fanout round the LED ring
 # _____________________________________________________________________________
 
+# imports
 import time
 import board
 import neopixel
 import json
 import digitalio
+from math import pi
 
 # LED setup
 pixel_pin = board.D18
@@ -22,158 +30,81 @@ button = digitalio.DigitalInOut(board.D4)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
-# Define pi
-pi = 3.14159265359
-
-# Declare memory
+# Declare memory between angles
 last_id = 0
-now_ms = 0
 last_ms = 0
-button_value = 0
 last_button = 0
 
-# Declare LED indices
-index_2 = 0
-index_1 = 0
-index0 = 0
-index1 = 0
-index2 = 0
-index3 = 0
+# Declare fanout
+fan_out = 3
 
-# Declare position and offset on LED ring and offset signedness
-ring_pos = 0
-ring_offset = 0
-offset_sign = 0
+# _____________________________________________________________________________
+# Sign function
+#
+# This outputs only the sign of a number ignoring the value
+# Possible outputs are 1 and -1
+# _____________________________________________________________________________
+def sign(num):
+    if num <= 0:
+        return -1
+    else:
+        return 1
 
 # _____________________________________________________________________________
 # LED ring code
+#
+# Calculates LED RGB values based on angle and amplitude
+# Fades out as it goes round the ring up to the fanout value
 # _____________________________________________________________________________
 def led_ring(angle, amplitude):
     
     # Translate angle to LED number
     ring_pos = round((angle * num_pixels) / (2 * pi))
+    
+    # Offset from Led number found above
     ring_offset = ((angle * num_pixels) / (2 * pi)) - ring_pos
+    
+    # For loop stepping through fan_out
+    for n in range(1 - fan_out, 1 + fan_out):
+        
+        # Calculate LED index based on angle
+        index = ring_pos + (n * sign(ring_offset)) + (num_pixels *
+                cmp(-ring_pos - (n * sign(ring_offset)), 0))
+        
+        # Extract current value of LED in question
+        pixel = list(pixels[index])
 
-    if ring_offset < 0:
-        offset_sign = -1
-    elif ring_offset > 0:
-        offset_sign = 1
-    else:
-        offset_sign = 0
-
-    ring_offset = abs(ring_offset)
+        # Calculate RGB values for LEDs incorporating:
+        # past value, amplitude and angle
+        pixels[index] = (( min(255, pixel[0] + round(amplitude * 255 *
+                           max(0, ((2 * amplitude) - 1)) * (fan_out - abs(n) +
+                           (abs(ring_offset) * sign(n)) ) / fan_out )),
+                           
+                           min(255, pixel[1] + round(amplitude * 255 *
+                           (1 - abs((2 * amplitude) - 1)) * (fan_out - abs(n) +
+                           (abs(ring_offset) * sign(n)) ) / fan_out )),
+                           
+                           min(255, pixel[2] + round(amplitude * 255 *
+                           max(0, ((-2 * amplitude) + 1)) * (fan_out - abs(n) +
+                           (abs(ring_offset) * sign(n)) ) / fan_out )) ))
     
-    # Work out LED values
-    if (ring_pos + (-2 * offset_sign)) < 0:
-        index_2 = ring_pos + (-2 * offset_sign) + num_pixels
-    elif (ring_pos + (-2 * offset_sign)) >= num_pixels:
-        index_2 = ring_pos + (-2 * offset_sign) - num_pixels
-    else:
-        index_2 = ring_pos + (-2 * offset_sign)
-    
-    pixel = list(pixels[index_2])
-
-    pixels[index_2] = (( min(255, pixel[0] + round(amplitude * 255 *
-        max(0, ((2 * amplitude) - 1)) * (1 - ring_offset) / 3 )),
-                         min(255, pixel[1] + round(amplitude * 255 *
-        (1 - abs((2 * amplitude) - 1)) * (1 - ring_offset) / 3 )),
-                         min(255, pixel[2] + round(amplitude * 255 *
-        max(0, ((-2 * amplitude) + 1)) * (1 - ring_offset) / 3 )) ))
-    
-    
-    if (ring_pos - offset_sign) < 0:
-        index_1 = ring_pos - offset_sign + num_pixels
-    elif (ring_pos - offset_sign) >= num_pixels:
-        index_1 = ring_pos - offset_sign - num_pixels
-    else:
-        index_1 = ring_pos - offset_sign
-    
-    pixel = list(pixels[index_1])
-    
-    pixels[index_1] = (( min(255, pixel[0] + round(amplitude * 255 *
-        max(0, ((2 * amplitude) - 1)) * (2 - ring_offset) / 3 )),
-                         min(255, pixel[1] + round(amplitude * 255 *
-        (1 - abs((2 * amplitude) - 1)) * (2 - ring_offset) / 3 )),
-                         min(255, pixel[2] + round(amplitude * 255 *
-        max(0, ((-2 * amplitude) + 1)) * (2 - ring_offset) / 3 )) ))
-    
-    
-    if ring_pos < 0:
-        index0 = ring_pos + num_pixels
-    elif ring_pos >= num_pixels:
-        index0 = ring_pos - num_pixels
-    else:
-        index0 = ring_pos
-    
-    pixel = list(pixels[index0])
-    
-    pixels[index0] = (( min(255, pixel[0] + round(amplitude * 255 *
-        max(0, ((2 * amplitude) - 1)) * (3 - ring_offset) / 3 )),
-                        min(255, pixel[1] + round(amplitude * 255 *
-        (1 - abs((2 * amplitude) - 1)) * (3 - ring_offset) / 3 )),
-                        min(255, pixel[2] + round(amplitude * 255 *
-        max(0, ((-2 * amplitude) + 1)) * (3 - ring_offset) / 3 )) ))
-    
-    
-    if (ring_pos + offset_sign) < 0:
-        index1 = ring_pos + offset_sign + num_pixels
-    elif (ring_pos + offset_sign) >= num_pixels:
-        index1 = ring_pos + offset_sign - num_pixels
-    else:
-        index1 = ring_pos + offset_sign
-    
-    pixel = list(pixels[index1])
-    
-    pixels[index1] = (( min(255, pixel[0] + round(amplitude * 255 *
-        max(0, ((2 * amplitude) - 1)) * (2 + ring_offset) / 3 )),
-                        min(255, pixel[1] + round(amplitude * 255 *
-        (1 - abs((2 * amplitude) - 1)) * (2 + ring_offset) / 3 )),
-                        min(255, pixel[2] + round(amplitude * 255 *
-        max(0, ((-2 * amplitude) + 1)) * (2 + ring_offset) / 3 )) ))
-    
-    
-    if (ring_pos + (2 * offset_sign)) < 0:
-        index2 = ring_pos + (2 * offset_sign) + num_pixels
-    elif (ring_pos + (2 * offset_sign)) >= num_pixels:
-        index2 = ring_pos + (2 * offset_sign) - num_pixels
-    else:
-        index2 = ring_pos + (2 * offset_sign)
-    
-    pixel = list(pixels[index2])
-    
-    pixels[index2] = (( min(255, pixel[0] + round(amplitude * 255 *
-        max(0, ((2 * amplitude) - 1)) * (1 + ring_offset) / 3 )),
-                        min(255, pixel[1] + round(amplitude * 255 *
-        (1 - abs((2 * amplitude) - 1)) * (1 + ring_offset) / 3 )),
-                        min(255, pixel[2] + round(amplitude * 255 *
-        max(0, ((-2 * amplitude) + 1)) * (1 + ring_offset) / 3 )) ))
-    
-    
-    if (ring_pos + (3 * offset_sign)) < 0:
-        index3 = ring_pos + (3 * offset_sign) + num_pixels
-    elif (ring_pos + (3 * offset_sign)) >= num_pixels:
-        index3 = ring_pos + (3 * offset_sign) - num_pixels
-    else:
-        index3 = ring_pos + (3 * offset_sign)
-    
-    pixel = list(pixels[index3])
-    
-    pixels[index3] = (( min(255, pixel[0] + round(amplitude * 255 *
-        max(0, ((2 * amplitude) - 1)) * ring_offset / 3 )),
-                        min(255, pixel[1] + round(amplitude * 255 *
-        (1 - abs((2 * amplitude) - 1)) * ring_offset / 3 )),
-                        min(255, pixel[2] + round(amplitude * 255 *
-        max(0, ((-2 * amplitude) + 1)) * ring_offset / 3 )) ))
-    
+    # Display LED values calculated
     pixels.show()
 
 # _____________________________________________________________________________
 # Main loop
+# Imports angle and amplitude value from json file, and triggers LED ring code
+# Fades the LED values by 1/3 each loop
+# Reads button value
+# Sends a calibrate command if short button press
+# Sends a reset command if long button press
 # _____________________________________________________________________________
 while True:
     
-    # Fade LEDs out
+    # Fade all LEDs out by 1/3 each time
     for i in range(0, num_pixels):
+        
+        # Convert from tuple to list for current LED
         pixel = list(pixels[i])
         if pixel[0] > 0:
             pixel[0] -= max(1, round(pixel[0]/3))
@@ -181,10 +112,14 @@ while True:
             pixel[1] -= max(1, round(pixel[1]/3))
         if pixel[2] > 0:
             pixel[2] -= max(1, round(pixel[2]/3))
+            
+        # Convert back from list to tuple for current LED
         pixels[i] = tuple(pixel)
+        
+        # Display LED values calculated
         pixels.show()
 
-    # Open Json file and trigger LED code
+    # Open Json file and read id, angle and amplitude
     with open('/tmp/chinchilla-backend.json', 'r') as json_file:
         for line in json_file.readlines():
             object = json.loads(line)
@@ -192,13 +127,22 @@ while True:
             angle = object['angle']
             amplitude = object['amplitude']
             
+            # Check id has increased (don't repeat same sound)
             if id > last_id:
+                
+                # Call LED ring code
                 led_ring(angle, amplitude)
+                
+                # Update last id memory
                 last_id = id
     
-    # Button code
+    # Get current time in milliseconds
     now_ms = int(time.time() * 1000)
+    
+    # Get current button value
     button_value = not button.value
+    
+    # Open file, write command and close
     f = open('/tmp/chinchilla-led-ctl', 'w')
     if (button_value and last_button == 0):
         last_ms = now_ms
@@ -207,5 +151,6 @@ while True:
     elif (button_value and now_ms > last_ms + 50 and last_button == 1):
         f.write('calibrate')
     f.close()
+    
+    # Update last button memory
     last_button = button_value
-
